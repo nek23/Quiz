2b81fd536b81e6fa7ebfccbfde5fec28bf03154d
Alex <applerama@live.ru>
2018-04-16 03:10:36 +0300

    все ок

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -1,25 +1,43 @@
 //
 //  ViewController.swift
-//  Quiz
+//  QuizFlag
 //
-//  Created by Alex on 16.04.2018.
+//  Created by Alex on 12.04.2018.
 //  Copyright © 2018 Alex. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var aboutUsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newGameButton.layer.cornerRadius = 5.0
+        aboutUsButton.layer.cornerRadius = 5.0
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func newGameButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5) {
+            self.newGameButton.transform = CGAffineTransform(scaleX: 2.5, y: 2.5)
+            self.newGameButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }
+        
     }
-
-
+    
+    @IBAction func aboutUsButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5) {
+            self.aboutUsButton.transform = CGAffineTransform(scaleX: 2.5, y: 2.5)
+            self.aboutUsButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }
+        
+    }
+    
+    @IBAction func unwindToNewGame(unwindSegue: UIStoryboardSegue) {
+    }
+    
 }
 
